97b89d3127e6c434b1d04e9894dfd21feed84ca0
Jelle Glebbeek <jw.glebbeek@gmail.com>
2020-12-11 01:27:46 +0100

    Make inputs persistent throughout server restarts

--- src/main/java/com/jelleglebbeek/huemc/Config.java
+++ src/main/java/com/jelleglebbeek/huemc/Config.java
@@ -1,28 +1,67 @@
 package com.jelleglebbeek.huemc;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Config {
 
     private final FileConfiguration cfg;
-    private final JavaPlugin pl;
+    private final FileConfiguration data;
+    private final File dataFile;
+    private final Main pl;
 
-    public Config(JavaPlugin pl) {
+    public Config(Main pl) {
         this.cfg = pl.getConfig();
+        this.dataFile = new File(pl.getDataFolder(), "data.yml");
+        this.data = YamlConfiguration.loadConfiguration(dataFile);
+
         this.pl = pl;
         cfg.addDefault("hue-bridge-ip", "");
         cfg.addDefault("api-key", "");
         cfg.options().copyDefaults(true);
         pl.saveConfig();
+
+    }
+
+    public void removeInputBlock(InputBlock block) {
+        for(String key : data.getKeys(false)) {
+            if(key.equals(block.getUUID())) {
+                data.set(key, null);
+            }
+        }
+        saveData();
+    }
+
+    public void saveAllInputBlocks() {
+        for(InputBlock inputBlock : pl.inputBlocks) {
+            data.set(inputBlock.getUUID(), inputBlock);
+        }
+        saveData();
+    }
+
+    public void loadAllInputBlocks() {
+        for(String key : data.getKeys(false)) {
+            pl.inputBlocks.add((InputBlock) data.get(key));
+        }
     }
 
     public void disable() {
+        saveAllInputBlocks();
         pl.saveConfig();
+        saveData();
     }
 
-    public FileConfiguration get() {
-        return this.cfg;
+    public FileConfiguration getConfig() { return this.cfg; }
+    public FileConfiguration getData() { return this.data; }
+    public void saveData() {
+        try {
+            data.save(dataFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
