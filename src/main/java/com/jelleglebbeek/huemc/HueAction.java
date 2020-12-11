ca6e826f759626f1cc7f5829204903239ec1a01a
Jelle Glebbeek <jw.glebbeek@gmail.com>
2020-12-11 01:27:22 +0100

    Implement middle layer between Hue and Spigot

---
+++ src/main/java/com/jelleglebbeek/huemc/HueAction.java
@@ -0,0 +1,8 @@
+package com.jelleglebbeek.huemc;
+
+public enum HueAction {
+    TURN_OFF,
+    TURN_ON,
+    BRIGHTNESS,
+    STATE
+}
--- src/main/java/com/jelleglebbeek/huemc/HueController.java
+++ src/main/java/com/jelleglebbeek/huemc/HueController.java
@@ -4,26 +4,24 @@ import io.github.zeroone3010.yahueapi.*;
 import io.github.zeroone3010.yahueapi.discovery.*;
 import org.bukkit.conversations.Conversable;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-public class HueAPI {
+public class HueController {
 
     private String ip;
     private String apiKey;
 
     private Hue hue;
 
-    public HueAPI(String ip, String apiKey) {
+    public HueController(String ip, String apiKey) {
         this.ip = ip;
         this.apiKey = apiKey;
     }
 
-    public HueAPI(String ip) {
+    public HueController(String ip) {
         this.ip = ip;
         this.apiKey = null;
     }
@@ -47,8 +45,8 @@ public class HueAPI {
             this.apiKey = key;
         }
         this.hue = new Hue(ip, apiKey);
-        cfg.get().set("api-key", apiKey);
-        cfg.get().set("hue-bridge-ip", ip);
+        cfg.getConfig().set("api-key", apiKey);
+        cfg.getConfig().set("hue-bridge-ip", ip);
         conv.sendRawMessage("HueMC is now connected to your bridge.");
     }
 
@@ -68,12 +66,52 @@ public class HueAPI {
         return zoneNames;
     }
 
-    public ArrayList<String> getLights(String name, boolean isZone) {
+    public ArrayList<String> getLightNames(String name, boolean isZone) {
         ArrayList<String> lightNames = new ArrayList<>();
         for(Light light : isZone ? hue.getZoneByName(name).get().getLights() : hue.getRoomByName(name).get().getLights()) {
             lightNames.add(light.getName());
         }
         return lightNames;
+    }
+
+    public Collection<Light> getLights(InputBlock inputBlock)  {
+        String areaName = inputBlock.getAreaName();
+        String lightName = inputBlock.getLightName();
+        boolean isZone = inputBlock.isZone();
+        if(lightName == null) {
+            if(isZone) {
+                return hue.getZoneByName(areaName).get().getLights();
+            } else {
+                return hue.getRoomByName(areaName).get().getLights();
+            }
+        } else {
+            Collection<Light> lights = new ArrayList<>();
+            if (isZone) {
+                lights.add(hue.getZoneByName(areaName).get().getLightByName(lightName).get());
+            } else {
+                lights.add(hue.getRoomByName(areaName).get().getLightByName(lightName).get());
+            }
+            return lights;
+        }
+    }
+
+    public void startAction(HueAction action, Collection<Light> lights, Object value)  {
+        switch(action) {
+            case TURN_OFF:
+                lights.forEach(Light::turnOff);
+                break;
+            case TURN_ON:
+                lights.forEach(Light::turnOn);
+                break;
+            case BRIGHTNESS:
+                if((int) value > 0) lights.forEach(Light::turnOn);
+                if((int) value == 0) lights.forEach(Light::turnOff);
+                lights.forEach(l -> l.setBrightness((int) value));
+                break;
+            case STATE:
+                lights.forEach(l -> l.setState((State) value));
+                break;
+        }
     }
 
     public void disable() {
