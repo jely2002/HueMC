69593ba5fd4fad7bd98e064e3346f12b209eb9fc
Jelle Glebbeek <jw.glebbeek@gmail.com>
2020-12-14 22:49:35 +0100

    Enable color changing with pistons

--- src/main/java/com/jelleglebbeek/huemc/InputListener.java
+++ src/main/java/com/jelleglebbeek/huemc/InputListener.java
@@ -11,11 +11,11 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.AnaloguePowerable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.block.BlockRedstoneEvent;
+import org.bukkit.event.block.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class InputListener implements Listener {
 
@@ -35,23 +35,20 @@ public class InputListener implements Listener {
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
+        if(pl.inputBlocks.size() == 0) return;
         if (hue == null) return;
         if(!e.getBlock().getType().toString().contains("GLASS") || e.getBlock().getType().toString().contains("PANE")) return;
         for(InputBlock inputBlock : pl.inputBlocks) {
             if(inputBlock.getLocation().equals(e.getBlock().getRelative(BlockFace.UP).getLocation())) {
-                if(e.getBlock().getType().toString().contains("GLASS") && !e.getBlock().getType().toString().contains("PANE")) {
-                    String dyeColor = e.getBlock().getType().toString().replace("_STAINED_GLASS", "");
-                    Color RGBcolor = DyeColor.valueOf(dyeColor).getColor();
-                    java.awt.Color HueColor = new java.awt.Color(RGBcolor.getRed(), RGBcolor.getGreen(), RGBcolor.getBlue());
-                    State lightState = State.builder().color(HueColor).keepCurrentState();
-                    Bukkit.getScheduler().runTaskAsynchronously(pl, () -> hue.startAction(HueAction.STATE, hue.getLights(inputBlock), lightState));
-                }
+                updateColor(e.getBlock(), inputBlock);
+                break;
             }
         }
     }
 
     @EventHandler
     public void onBlockDestroy(BlockBreakEvent e) {
+        if(pl.inputBlocks.size() == 0) return;
         if(!pl.cfg.getConfig().getBoolean("white-light-when-no-glass")) return;
         if (hue == null) return;
         if(!e.getBlock().getType().toString().contains("GLASS") || e.getBlock().getType().toString().contains("PANE")) return;
@@ -62,13 +59,42 @@ public class InputListener implements Listener {
                     inputBlock.setColor(java.awt.Color.WHITE);
                     Bukkit.getScheduler().runTaskAsynchronously(pl, () -> hue.startAction(HueAction.STATE, hue.getLights(inputBlock), lightState));
                 }
+                break;
             }
         }
     }
 
-    private void updateColor(InputBlock inputBlock) {
-        Block colorBlock = inputBlock.getLocation().getBlock().getRelative(BlockFace.DOWN);
-        if(colorBlock.getType().toString().contains("GLASS") && !colorBlock.getType().toString().contains("PANE")) {
+    @EventHandler
+    public void onPistonExtend(BlockPistonExtendEvent e) {
+        if(pl.inputBlocks.size() == 0) return;
+        if(e.getBlocks().size() == 0) return;
+        updatePiston(e.getBlocks(), e.getDirection());
+    }
+
+    @EventHandler
+    public void onPistonRetract(BlockPistonRetractEvent e) {
+        if(pl.inputBlocks.size() == 0) return;
+        if(e.getBlocks().size() == 0) return;
+        updatePiston(e.getBlocks(), e.getDirection());
+    }
+
+    private void updatePiston(List<Block> blocks, BlockFace direction) {
+        upper:
+        for(Block block : blocks) {
+            if(!block.getType().toString().contains("STAINED_GLASS") || block.getType().toString().contains("PANE")) continue;
+            for(InputBlock inputBlock : pl.inputBlocks) {
+                if(inputBlock.getLocation().equals(block.getRelative(BlockFace.UP).getRelative(direction).getLocation())) {
+                    Bukkit.getScheduler().runTaskLater(pl, () -> updateColor(block.getRelative(direction), inputBlock), 3);
+                    break upper;
+                } else if (block.getRelative(direction).getLocation().equals(inputBlock.getLocation().getBlock().getRelative(BlockFace.DOWN).getRelative(direction).getLocation())) {
+                    Bukkit.getScheduler().runTaskLater(pl, () -> updateColor(inputBlock.getLocation().getBlock().getRelative(BlockFace.DOWN), inputBlock), 3);
+                }
+            }
+        }
+    }
+
+    private void updateColor(Block colorBlock, InputBlock inputBlock) {
+        if(colorBlock.getType().toString().contains("STAINED_GLASS") && !colorBlock.getType().toString().contains("PANE")) {
             String dyeColor = colorBlock.getType().toString().replace("_STAINED_GLASS", "");
             Color RGBcolor = DyeColor.valueOf(dyeColor).getColor();
             java.awt.Color HueColor = new java.awt.Color(RGBcolor.getRed(), RGBcolor.getGreen(), RGBcolor.getBlue());
@@ -89,6 +115,7 @@ public class InputListener implements Listener {
 
     @EventHandler
     public void onRedstoneChange(BlockRedstoneEvent e) {
+        if(pl.inputBlocks.size() == 0) return;
         if (hue == null) return;
         if (e.getBlock().getType().equals(Material.REDSTONE_LAMP)) {
             for (InputBlock inputBlock : pl.inputBlocks) {
@@ -118,7 +145,7 @@ public class InputListener implements Listener {
                         }
                         Bukkit.getScheduler().runTaskAsynchronously(pl, () -> {
                             hue.startAction(HueAction.BRIGHTNESS, hue.getLights(inputBlock), brightness);
-                            updateColor(inputBlock);
+                            updateColor(inputBlock.getLocation().getBlock().getRelative(BlockFace.DOWN), inputBlock);
                         });
                         break;
 
@@ -127,12 +154,12 @@ public class InputListener implements Listener {
                         if (e.getNewCurrent() > 0 && e.getOldCurrent() == 0) {
                             Bukkit.getScheduler().runTaskAsynchronously(pl, () -> {
                                 hue.startAction(HueAction.TURN_ON, hue.getLights(inputBlock), null);
-                                updateColor(inputBlock);
+                                updateColor(inputBlock.getLocation().getBlock().getRelative(BlockFace.DOWN), inputBlock);
                             });
                         } else if (e.getNewCurrent() == 0 && e.getOldCurrent() > 0) {
                             Bukkit.getScheduler().runTaskAsynchronously(pl, () -> {
                                 hue.startAction(HueAction.TURN_OFF, hue.getLights(inputBlock), null);
-                                updateColor(inputBlock);
+                                updateColor(inputBlock.getLocation().getBlock().getRelative(BlockFace.DOWN), inputBlock);
                             });
                         }
                         break;
@@ -147,7 +174,7 @@ public class InputListener implements Listener {
                     InputBlock inputBlock = dimmerCombination.get(e.getBlock());
                     AnaloguePowerable powerable = (AnaloguePowerable) e.getBlock().getBlockData();
                     int brightness = (int) Math.round((double) powerable.getPower() / 15d * 100d);
-                    updateColor(inputBlock);
+                    updateColor(null, inputBlock);
                     Bukkit.getScheduler().runTaskAsynchronously(pl, () -> hue.startAction(HueAction.BRIGHTNESS, hue.getLights(inputBlock), brightness));
                     powerlineChange = false;
                 }, 3);
